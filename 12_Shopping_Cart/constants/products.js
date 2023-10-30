bfd910f29d502829594b5128b85299e85cab4358
GnapikaM <108170321+GnapikaM@users.noreply.github.com>
2023-10-30 19:23:44 +0530

    Add files via upload

---
+++ 12_Shopping_Cart/constants/products.js
@@ -0,0 +1,81 @@
+import { product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11 } from "../assets"
+
+export const products = [
+    {
+        id: 1,
+        productImage: product1,
+        productName: "Little girl Witch Costume With Pumpkin",
+        price: "4599",
+        rating: "4.5",
+    },
+    {
+        id: 2,
+        productImage: product11,
+        productName: "Halloween Pumpkin, Witch hat, Broomstick",
+        price: "679",
+        rating: "4.3",
+    },
+    {
+        id: 3,
+        productImage: product5,
+        productName: "Young Girl Black and White Witch Dress",
+        price: "5200",
+        rating: "4.8",
+    },
+    {
+        id: 4,
+        productImage: product6,
+        productName: "Young Boy Blue Witch Dress",
+        price: "1999",
+        rating: "4.2",
+    },
+    {
+        id: 5,
+        productImage: product4,
+        productName: "Little girl Witch Black Long Dress",
+        price: "1500",
+        rating: "4.7",
+    },
+    {
+        id: 6,
+        productImage: product7,
+        productName: "Creepy Halloween white cloth ghost",
+        price: "999",
+        rating: "5.0",
+    },
+    {
+        id: 7,
+        productImage: product8,
+        productName: "Trick or Treat Buckets with Trolley",
+        price: "600",
+        rating: "4.5",
+    },
+    {
+        id: 8,
+        productImage: product2,
+        productName: "Little girl Witch Costume With Pumpkin",
+        price: "4300",
+        rating: "4.3",
+    },
+    {
+        id: 9,
+        productImage: product9,
+        productName: "Halloween Monster face balloons",
+        price: "299",
+        rating: "4.5",
+    },
+    {
+        id: 10,
+        productImage: product3,
+        productName: "Little girl Witch Costume With Pumpkin",
+        price: "5000",
+        rating: "4.1",
+    },
+    {
+        id: 11,
+        productImage: product10,
+        productName: "Halloween monster face lamp",
+        price: "199",
+        rating: "4.9",
+    }
+]
